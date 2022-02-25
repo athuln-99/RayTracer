debe5a9a7ec709f5ac556909189f7a96cddf5d37
athuln-99 <63635278+athuln-99@users.noreply.github.com>
2022-02-25 17:28:30 +0100

    files

---
+++ build/CMakeFiles/ray.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/image.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/image.cpp.o"
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/lode/lodepng.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/lode/lodepng.cpp.o"
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/main.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/main.cpp.o"
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/objloader.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/objloader.cpp.o"
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/raytracer.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/raytracer.cpp.o"
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/scene.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/scene.cpp.o"
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/shapes/cylinder.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/shapes/cylinder.cpp.o"
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/shapes/mesh.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/shapes/mesh.cpp.o"
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/shapes/quad.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/shapes/quad.cpp.o"
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/shapes/sphere.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/shapes/sphere.cpp.o"
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/shapes/triangle.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/shapes/triangle.cpp.o"
+  "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/Code/triple.cpp" "/mnt/d/University/3rd Year/Computer Graphics/OpenGL/RayTracer_1/build/CMakeFiles/ray.dir/Code/triple.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
